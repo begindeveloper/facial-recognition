1841e3818790e8d9c4de4c6094ff4d0badb771aa
begindeveloper <107010469+begindeveloper@users.noreply.github.com>
2023-07-05 11:39:16 -0500

    updated 05-July-2023

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const video = document.getElementById('video')
 
 Promise.all([
-    faceapi.nets.tnyFaceDetector.loadFromUri('')
-])
+    faceapi.nets.TinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo)
 
 function startVideo() {
     navigator.getUserMedia(
@@ -13,4 +16,11 @@ function startVideo() {
     )
 }
 
-startVideo()
+video.addEventListener('play', () => {
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video,
+            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks
+            ().withFaceExpressions()
+            console.log(detections)
+    }, 100)
+})
